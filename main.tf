0077f31b315187faff6be5c4f69ce9c504c2879b
kulalsooraj <53329323+kulalsooraj@users.noreply.github.com>
2025-02-02 18:55:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ count = 5
 ami = "ami-0ddc798b3f1a5117e"
 instance_type = "t2.medium"
 key_name = "test"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
+vpc_security_group_ids = ["sg-005381e18d32367b4"]
 tags = {
 Name = var.instance_names[count.index]
 }
